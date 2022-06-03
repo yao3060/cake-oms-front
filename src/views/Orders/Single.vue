e9f60d2674b682d15be9f212e98a795cdef357b3
caojun <105859214+2022caojun@users.noreply.github.com>
2022-06-03 18:55:24 +0700

    Update Single.vue

--- src/views/Orders/Single.vue
+++ src/views/Orders/Single.vue
@@ -27,7 +27,7 @@
         <nut-cell class="small-cell" title="订货门店" :desc="order.store_name" />
 
         <Creator :id="order.id" :creator="order.creator" :store-id="order.store_id" />
-        <PickupMethod :id="order.id" style="color:red" :creator="order.creator" :value="order.pickup_method" />
+        <PickupMethod :id="order.id" :creator="order.creator" :value="order.pickup_method" />
         <PickupTime :id="order.id" :creator="order.creator" :value="order.pickup_time" />
 		
         <nut-cell class="small-cell" style="color:red" title="出品时间" :desc="order.pickup_time" />		
